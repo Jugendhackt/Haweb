bcd52b01b9faa5560c4388469639bf1f8e32016d
BuildTools <unconfigured@null.spigotmc.org>
2018-07-13 19:02:25 +0200

    Change Base Cgi to Login Cgi

--- client/cgi-bin/login.py
+++ client/cgi-bin/login.py
@@ -2,26 +2,37 @@
 
 # Import modules for CGI handling
 import cgi, cgitb
+# Import Date for Log
+import datetime
+now = datetime.datetime.now()
+#Import Hashlib for creating hashes
+import hashlib
+
+log = open("/home/anton/Dokumente/Haweb/client/cgi-bin/data/log.txt","a")
 
 # Create instance of FieldStorage
 form = cgi.FieldStorage()
 
 # Get data from fields
-first_name = form.getvalue('first_name')
-last_name  = form.getvalue('last_name')
+username = form.getvalue('username')
+password  = form.getvalue('password')
 
 print "Content-type:text/html\r\n\r\n"
 print "<html>"
 print "<head>"
-print "<title>Hello - Second CGI Program</title>"
+print "<title>Login</title>"
 print "</head>"
 print "<body>"
-print "<h2>Hello %s %s</h2>" % (first_name, last_name)
+print "<h2>Only Post</h2>"
 print "</body>"
 print "</html>"
 
-l = open("/home/anton/Dokumente/Haweb/client/cgi-bin/data/log.txt","a")
+log.write("["+str(now)+"] Login of "+ str(username)+"\n")
 
-l.write("First_name:"+str(first_name)+"  Last_name:"+ str(last_name)+"\n")
+mystring = "hallo"
 
-l.close()
+hashm = hashlib.md5(mystring.encode())
+
+log.write(str(hashm.hexdigest())+"\n")
+
+log.close()
