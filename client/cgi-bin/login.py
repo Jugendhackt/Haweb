2a593ad325a2d050441927d8cdce992645e54330
BuildTools <unconfigured@null.spigotmc.org>
2018-07-18 22:10:22 +0200

    Changed Anwser

--- client/cgi-bin/login.py
+++ client/cgi-bin/login.py
@@ -7,6 +7,11 @@ import datetime
 now = datetime.datetime.now()
 #Import Hashlib for creating hashes
 import hashlib
+#Import Database
+import sqlite3
+db = sqlite3.connect('/home/anton/Dokumente/Haweb/client/othery/database.sqlite3')
+dbc = db.cursor()
+
 
 log = open("/home/anton/Dokumente/Haweb/client/cgi-bin/data/log.txt","a")
 
@@ -20,14 +25,16 @@ password  = form.getvalue('password')
 print "Content-type:text/html\r\n\r\n"
 print "<html>"
 print "<head>"
-print "<title>Login</title>"
+print "<title>Error response</title>"
 print "</head>"
 print "<body>"
-print "<h2>Only Post</h2>"
+print "<h1>Error response</h1>"
+print "Error code 204 </br></br> Message: No Content </br> </br>"
+print "Error code explanation: 204 = The server successfully processed the request and is not returning any content"
 print "</body>"
 print "</html>"
 
-log.write("["+str(now)+"] Login of "+ str(username)+"\n")
+hashed_password = hash_password(new_pass)
 
 def hash_password(password):
     # uuid is used to generate a random number
@@ -38,4 +45,9 @@ def check_password(hashed_password, user_password):
     password, salt = hashed_password.split(':')
     return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
 
-hashed_password = hash_password(new_pass)
+
+
+if check_password(db_password,password):
+    log.write("["+str(now)+"] Login of "+ str(username)+"\n")
+
+log.close
