fd8865540b1fc3ce578736cfa2c346119bda4c6b
BuildTools <unconfigured@null.spigotmc.org>
2018-07-20 22:18:30 +0200

    Added Esc to Close Window

--- client/js/settings.js
+++ client/js/settings.js
@@ -13,3 +13,9 @@ window.onmouseup = function(event) {
         modal.style.display = "none";
     }
 }
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 27) {
+        closesettings();
+    }
+};
