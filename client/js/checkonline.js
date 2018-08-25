302b3bda7b334cab4f905c494362165ede5a6447
BuildTools <unconfigured@null.spigotmc.org>
2018-08-25 16:52:40 +0200

    Interval Removed because not Working

--- client/js/checkonline.js
+++ client/js/checkonline.js
@@ -1,13 +1,8 @@
-var offi ;
 function offFunction() {
-    offi = setInterval(2000,function (){
-        malert("Du bist Offline");
-    })
+    malert("Du bist Offline");
 }
-
 function onFunction() {
-    clearInterval(offi)
     malert("Du bist wieder Online");
 }
 window.addEventListener("online",onFunction())
-window.addEventListener("offline",offFunction())
+window.addEventListener("offline",offFunction())    
