940a29b2df2a8363b18170514ee55e46bf74260b
BuildTools <unconfigured@null.spigotmc.org>
2018-08-25 16:51:23 +0200

    -

--- client/js/alerts.js
+++ client/js/alerts.js
@@ -1,12 +1,12 @@
-function malert(text) {
-    // Get the snackbar DIV
-    var alertbox = document.getElementById("alerts");
-    var alert = document.createElement('div');
-    // Add the "show" class to DIV
-    alert.className = "show";
-    alert.id = "snackbar";
-    alert.innerHTML = text;
-    alertbox.appendChild(alert);
-    // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){ alert.className = alert.className.replace("show", ""); }, 3000);
-}
+    function malert(text) {
+        // Get the snackbar DIV
+        var alertbox = document.getElementById("alerts");
+        var alert = document.createElement('div');
+        // Add the "show" class to DIV
+        alert.className = "show";
+        alert.id = "snackbar";
+        alert.innerHTML = text;
+        alertbox.appendChild(alert);
+        // After 3 seconds, remove the show class from DIV
+        setTimeout(function(){ alert.className = alert.className.replace("show", ""); }, 3000);
+    }
