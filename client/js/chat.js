905d5986f96bce06f6f8198ed80480b8164b6684
BuildTools <unconfigured@null.spigotmc.org>
2018-08-26 15:11:31 +0200

    Added Missing ";"

--- client/js/chat.js
+++ client/js/chat.js
@@ -5,21 +5,21 @@
     mySocket.onmessage = function (event) {
         var output = document.getElementById("chatcontent");
         // put text into our output div
-        var newmessage = JSON.parse(event.data)
+        var newmessage = JSON.parse(event.data);
         console.log(newmessage);
         
         var newmessage_html = document.createElement('div');
         newmessage_html.innerHTML = '<div class="chatcontent"><div class="container"><span>'+newmessage.message.user+'</span><p>'+newmessage.message.text+'</p><span class="time">'+newmessage.message.time+'</span></div>';
         output.appendChild(newmessage_html);
-    }
+    };
     var formchat = document.getElementById("chatsend");
     var input = document.getElementById("input"); 
     formchat.addEventListener("submit", function (e) {
         // on forms submission send input to our server
         input_text = input.value;
         mySocket.send(input_text);
-        e.preventDefault()
+        e.preventDefault();
         input.value = "";
         return false;
- })
+ });
 });
