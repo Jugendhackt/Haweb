47f6ae5dae31ce71678983898def7a31e5274f5c
BuildTools <unconfigured@null.spigotmc.org>
2018-08-27 14:04:21 +0200

    Fixed | Removed Time Function

--- client/ws/server.py
+++ client/ws/server.py
@@ -8,8 +8,6 @@ import time
 class WSHandler(tornado.websocket.WebSocketHandler):
     clients = []
     messages = []
-    def gettime(self):
-        return str(time.strftime('%H:%M:%S', time.localtime()))
     def open(self):
         self.clients.append(self)
         print '[Server] New connection'
@@ -17,7 +15,7 @@ class WSHandler(tornado.websocket.WebSocketHandler):
             self.write_message(message)
     def on_message(self, message):
         clientadress = self.request.remote_ip
-        print '[Chat] '+self.gettime+" "+clientadress+' %s' % message
+        print '[Chat] '+clientadress+' %s' % message
         self.sendall(message,clientadress)
         self.messages.append(self.makejsonmessage(message,clientadress))
     def on_close(self):
@@ -27,7 +25,8 @@ class WSHandler(tornado.websocket.WebSocketHandler):
         return True
     def makejsonmessage(self,message,name=""):
         message = message.replace('"',"'")
-        return '{"message":{ "user":"'+name+'","text":"'+message+'","time":"'+self.gettime+'"}}'
+        time_m = str(time.strftime('%H:%M:%S', time.localtime()))
+        return '{"message":{ "user":"'+name+'","text":"'+message+'","time":"'+time_m+'"}}'
     def sendall(self,message,name=""):
         for client in self.clients:
             client.write_message(self.makejsonmessage(message,name))
