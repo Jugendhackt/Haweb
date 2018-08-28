f713218f8ac33c3148f704a732363b460821c21a
Julius Schoder <julius@schoder.eu>
2018-08-28 17:30:15 +0200

    Key generator fixed

--- server/security/Keys/Key_Generator.py
+++ server/security/Keys/Key_Generator.py
@@ -3,7 +3,6 @@ import random
 abc = ['q', 'w', 'e', 'r', 't', 'z', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'y', 'x', 'c', 'v', 'b', 'n', 'm']
 num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 key = ''
-
 for i in range (20):
     rand = random.randint(1, 2)
     if rand == 1:
@@ -13,3 +12,9 @@ for i in range (20):
         randnum = random.choice(num)
         key = key + randnum
 print key
+path =  'keysfile.txt'
+file = open(path, 'a')
+file.write(key)
+file.write('\n')
+file.close
+
