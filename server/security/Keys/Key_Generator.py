49edd829a19b459422572f9134879cb80b97ba78
Julius Schoder <julius@schoder.eu>
2018-08-28 17:16:18 +0200

    Updates

---
+++ server/security/Keys/Key_Generator.py
@@ -0,0 +1,15 @@
+import random
+
+abc = ['q', 'w', 'e', 'r', 't', 'z', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'y', 'x', 'c', 'v', 'b', 'n', 'm']
+num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+key = ''
+
+for i in range (20):
+    rand = random.randint(1, 2)
+    if rand == 1:
+        randabc = random.choice(abc)
+        key = key + randabc
+    else:
+        randnum = random.choice(num)
+        key = key + randnum
+print key
