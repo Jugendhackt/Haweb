50b523adb018c947f5b0a3e4445d15b7035b158d
BuildTools <unconfigured@null.spigotmc.org>
2018-09-07 14:35:59 +0200

    No more icon path

--- client/js/menu.js
+++ client/js/menu.js
@@ -1,5 +1,6 @@
-function mobliebutton() {
+function mobliebutton(y) {
     var x = document.getElementById("myTopnav");
+    y.classList.toggle("change");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
