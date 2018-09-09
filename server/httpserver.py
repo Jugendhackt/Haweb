10d4befd252b6eb098a220f985026546691503b3
BuildTools <unconfigured@null.spigotmc.org>
2018-09-09 11:02:34 +0200

    / = index.html

--- server/httpserver.py
+++ server/httpserver.py
@@ -7,13 +7,15 @@ class MainHandler(tornado.web.RequestHandler):
         self.set_header("Server","")
     def get(self,url):
         try:
-            print ("Get "+url)
             if(url.endswith(".css")):
                 self.set_header("Content-Type","text/css")
             if(url.endswith(".js")):
                 self.set_header("Content-Type","text/javascript")
             if(url.endswith(".svg")):
                 self.set_header("Content-Type","application/svg+xml")
+            if(url == ""):
+                url = "index.html"
+            print ("Get "+url)
             self.render("../client/"+url)
         except:
             self.render("error.html")
