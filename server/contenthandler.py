5a6b79e334bf26a9fc002c7b1bbb86a3af605f96
AntonK123 <anton@akraft.org>
2018-09-26 21:11:20 +0200

    First Part of Content Handler

--- server/contenthandler.py
+++ server/contenthandler.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
 
 def tab(lang,logined):
-    return True;
+    if logined == True:
+       tabs = ("Home","Know-How","Homework","Chat") 
+    else:
+        tabs = ("Home","Know-How")
+    return '{"type":"tabs","Tabs":{"tab1":"%s","tab2":"%s","tab3":"%s","tab4":"%s"}}' %tabs
