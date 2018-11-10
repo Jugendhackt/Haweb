3abf9acffceec95d0f2e81b2077ae0d590b86f6c
AntonK123 <anton@akraft.org>
2018-11-10 14:32:06 +0100

    Updater

--- server/contenthandler.py
+++ server/contenthandler.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 
-def tab(lang,logined):
-    if logined == True:
-       tabs = ("Home","Know-How","Homework","Chat") 
-    else:
-        tabs = ("Home","Know-How")
+def tabs(lang,logined):
+    tabs = ("Home","Know-How","Homework","Chat") 
     return '{"type":"tabs","Tabs":{"tab1":"%s","tab2":"%s","tab3":"%s","tab4":"%s"}}' %tabs
