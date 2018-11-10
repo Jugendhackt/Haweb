d38f2e9a6a00d800bbfab48fc7ceb2c49dbdd8de
AntonK123 <anton@akraft.org>
2018-11-10 14:32:49 +0100

    Ups

--- server/contenthandler.py
+++ server/contenthandler.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-def tabs(lang,logined):
+def tabs():
     tabs = ("Home","Know-How","Homework","Chat") 
     return '{"type":"tabs","Tabs":{"tab1":"%s","tab2":"%s","tab3":"%s","tab4":"%s"}}' %tabs
