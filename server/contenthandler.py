c542e9cb6cf400b926c0ecbf31cfdce136f79d29
AntonK123 <anton@akraft.org>
2018-11-10 15:54:42 +0100

    Started New Content Manager

--- server/contenthandler.py
+++ server/contenthandler.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
-
+from xml.dom import minidom
+import os
+os.chdir("../client-html")
 def tabs():
     tabs = ("Home","Know-How","Homework","Chat") 
-    return '{"type":"tabs","Tabs":{"tab1":"%s","tab2":"%s","tab3":"%s","tab4":"%s"}}' %tabs
+    return '{"type":"tabs","Tabs":{"home":"%s","know-how":"%s","homework":"%s","chat":"%s"}}' %tabs
+def content():
+    contents = []
+    for filepath in ("test"):
+        filepath = filepath.lower()+"/index.html"
+        html = minidom.parse(filepath)
+        contents.append(html.getElementById("content"))
+    return '{"type":"content","contents":{"home":"%s","know-how":"%s","homework":"%s","chat":"%s"}}' %contents
